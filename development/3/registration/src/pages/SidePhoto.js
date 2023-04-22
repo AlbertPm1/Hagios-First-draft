3997b48f39fd84d7ee5e87c10cda9aef0ceba6f9
Albertpm1 <petermburunganga98@gmail.com>
2023-04-22 13:57:15 +0300

    changed the styling

--- development/3/registration/src/pages/SidePhoto.js
+++ development/3/registration/src/pages/SidePhoto.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import myPhoto3 from '../images/myPhoto3.jpg'; // import the image file
 
 function SidePhoto() {
   return (
-    <img src="https://scontent.fnbo13-1.fna.fbcdn.net/v/t39.30808-6/340418112_524533522978272_2809219133693312277_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHjEQBEt2u0xMEMXxjIrYSYpyP2KV1hWSinI_YpXWFZKKuNbtFUKRdB7vkG64bETdbxDN6ILwEGpcOcIycOO6We&_nc_ohc=mKgS-AGWLhoAX9QljNp&_nc_zt=23&_nc_ht=scontent.fnbo13-1.fna&oh=00_AfAeinXVy7DAfXCSVgDTdOBFSOFtpa8e4P0ZFqaLQvjQqQ&oe=6442C956" alt="" className="side-photo" />
+    <img src={require('../images/myPhoto3.jpg')} alt="SidePhoto" className="side-photo" />
   );
 }
 
